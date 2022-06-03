e27894c7c345f058bf108f8364d02191a93b7926
qorbaxk <bekgyber3@hanmail.net>
2022-06-03 22:53:05 +0900

    6/3 start

---
+++ src/component/ContactForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Form, Button} from 'react-bootstrap';
+
+const ContactForm = () => {
+  return (
+    <div>
+        <Form>
+            <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>이름</Form.Label>
+                <Form.Control type="text" placeholder="이름을 입력하세요." />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formContact">
+                <Form.Label>전화번호</Form.Label>
+                <Form.Control type="number" placeholder="전화번호를 입력하세요." />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+                저장
+            </Button>
+        </Form>
+    </div>
+  )
+}
+
+export default ContactForm
